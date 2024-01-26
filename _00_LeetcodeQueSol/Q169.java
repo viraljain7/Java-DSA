30b8071b934600d6cead09662d1c4067f99870b7
viraljain7 <veerljain1234@gmail.com>
2024-01-26 23:27:25 +0530

    26 jan

--- _00_LeetcodeQueSol/Q169.java
+++ _00_LeetcodeQueSol/Q169.java
@@ -1,9 +1,27 @@
 package _00_LeetcodeQueSol;
-import java.util.Arrays;
+import java.util.*;
 public class Q169 {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+       // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+
+
+        HashMap<Integer, Integer> map= new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++)
+        {
+            if(map.containsKey(nums[i]))
+                map.put(nums[i], map.get(nums[i])+1);
+            else
+                map.put(nums[i],1);
+        }
+        for(int key: map.keySet())
+        {
+            if(map.get(key) > n/2)
+                return key;
+        }
+        return -1;
     }
 
 }
