61e88a6f5078bc1339f55799a51bf8f9bc88abe3
Viral Jain <veerljain1234@gmail.com>
2024-02-06 22:44:16 +0530

    6feb

---
+++ _00_LeetcodeQueSol/Q151.java
@@ -0,0 +1,15 @@
+package _00_LeetcodeQueSol;
+
+public class Q151 {
+    class Solution {
+        public String reverseWords(String s) {
+            String ans = "";
+            String[] words = s.split("\\s+");
+            for (String w : words) {
+                if (ans == "") ans = w;
+                else ans = w + " " + ans;
+            }
+            return ans;
+        }
+    }
+}
