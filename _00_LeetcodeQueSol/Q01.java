7bf053b1b8511fd7736c211e9d3e2a1e0693172e
viraljain7 <veerljain1234@gmail.com>
2024-03-24 23:01:03 +0530

    q1

--- _00_LeetcodeQueSol/Q01.java
+++ _00_LeetcodeQueSol/Q01.java
@@ -19,4 +19,20 @@ public class Q01 {
         return finalRes;
     }
 
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0 ;i<nums.length;i++){
+            int n=nums[i];
+            int next=target-nums[i];
+
+            if(mp.containsKey(next)){
+                return new int[]{i,mp.get(next)};
+            }
+
+            mp.put(nums[i],i);
+        }
+
+        return new int[]{};
+    }
+
 }
