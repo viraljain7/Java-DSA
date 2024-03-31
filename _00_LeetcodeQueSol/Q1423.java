3123f8ad6c0ed56a33da5ba7e44c0f69377c9ef2
viraljain7 <veerljain1234@gmail.com>
2024-03-31 23:53:57 +0530

    updated

--- _00_LeetcodeQueSol/Q1423.java
+++ _00_LeetcodeQueSol/Q1423.java
@@ -1,2 +1,31 @@
-package _00_LeetcodeQueSol;public class Q1423 {
+package _00_LeetcodeQueSol;
+
+public class Q1423 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int maxScore = 0;
+
+        // Sum the first k elements in our window
+        for (int i = 0; i < k; i++) {
+            maxScore += cardPoints[i];
+        }
+
+        int currentScore = maxScore;
+        for (int i = k - 1; i >= 0; i--) {
+            // We remove the last visited element and add the non-visited element from the end
+            currentScore -= cardPoints[i];
+            currentScore += cardPoints[n - k + i];
+
+
+            // We check the maximum value any possible combination can give
+            maxScore = Math.max(maxScore, currentScore);
+        }
+
+        return maxScore;
+    }
+
 }
--- _00_LeetcodeQueSol/Q48.java
+++ _00_LeetcodeQueSol/Q48.java
@@ -1,2 +1,33 @@
-package _00_LeetcodeQueSol;public class Q48 {
+package _00_LeetcodeQueSol;
+
+public class Q48 {
+    public static void main(String[] args) {
+
+    }
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        for(int[] arr : matrix)
+            reverse(arr);
+    }
+
+    public void transpose(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public void reverse(int[] arr){
+        int i = 0, j = arr.length-1;
+        while(i <= j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
